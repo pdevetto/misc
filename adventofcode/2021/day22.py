6800c35c93ad522f56ff548f107ce4f2ac441ddb
Pierre De Vettor <pierredevettor@gmail.com>
2021-12-23 12:26:51 +0100

    [Advent2021] Day 22 - Cleanup

--- adventofcode/2021/day22.py
+++ adventofcode/2021/day22.py
@@ -6,40 +6,6 @@ def readfile(path):
     with open(path) as f:
         content = f.read().splitlines()
         return content
-def pf(k):
-    #print(k)
-    [x,y,z] = k.split(',')
-    return int(x), int(y), int(z)
-def fp(x,y,z):
-    return str(x) + ',' + str(y) + ',' + str(z)
-
-data_exo = readfile("C:\\Users\\amlaw\\data\\d22.txt")
-data_exemple=["nope",
-"on x=-20..26,y=-36..17,z=-47..7",
-"on x=-20..33,y=-21..23,z=-26..28",
-"on x=-22..28,y=-29..23,z=-38..16",
-"on x=-46..7,y=-6..46,z=-50..-1",
-"on x=-49..1,y=-3..46,z=-24..28",
-"on x=2..47,y=-22..22,z=-23..27",
-"on x=-27..23,y=-28..26,z=-21..29",
-"on x=-39..5,y=-6..47,z=-3..44",
-"on x=-30..21,y=-8..43,z=-13..34",
-"on x=-22..26,y=-27..20,z=-29..19",
-"off x=-48..-32,y=26..41,z=-47..-37",
-"on x=-12..35,y=6..50,z=-50..-2",
-"off x=-48..-32,y=-32..-16,z=-15..-5",
-"on x=-18..26,y=-33..15,z=-7..46",
-"off x=-40..-22,y=-38..-28,z=23..41",
-"on x=-16..35,y=-41..10,z=-47..6",
-"off x=-32..-23,y=11..30,z=-14..3",
-"on x=-49..-5,y=-3..45,z=-29..18",
-"off x=18..30,y=-20..-8,z=-3..13",
-"on x=-41..9,y=-7..43,z=-33..15",
-"on x=-54112..-39298,y=-85059..-49293,z=-27449..7877",
-"on x=967..23432,y=45373..81175,z=27513..53682"]
-
-#print(data_exemple)
-#print(data_exo)
 
 def procedure(data):
     engine = {}
@@ -59,10 +25,6 @@ def procedure(data):
                             if engine.get(k,False):
                                 del engine[k]
     return engine
-                            
-#eng = procedure(data_exo)
-
-data_exemple = readfile("C:\\Users\\amlaw\\data\\d22_exemple.txt")
 
 def overlap_comp(n1,n2,m1,m2):
     b1,b2 = 0,0
@@ -252,29 +214,21 @@ base = [(0,1),(2,4),(5,6)]
 result = [ [x1,x2,y1,y2,z1,z2] for (x1,x2) in base for (y1,y2) in base for (z1,z2) in base
          if [x1,x2,y1,y2,z1,z2] != [2,4,2,4,2,4]]
 realresult = overlap([0,6,0,6,0,6], [2,4,2,4,2,4], False)
-
 assert compt(result) == 316
 assert compt(realresult) == 316
-print("overlap([0,6,0,6,0,6], [6,7,0,6,0,6], False)")
 assert overlap([0,6,0,6,0,6], [6,7,0,6,0,6], False) == [[0, 5, 0, 6, 0, 6]]
-print("overlap([0,3,0,3,0,3], [0,10,0,2,0,3], True)")
 assert overlap([0,3,0,3,0,3], [0,10,0,2,0,3], True) == [[0, 3, 3, 3, 0, 3]]
-print("overlap([0,3,0,3,0,3], [0,10,0,4,0,3], True)", overlap([0,3,0,3,0,3], [0,10,0,4,0,3], True))
 assert overlap([0,3,0,3,0,3], [0,10,0,4,0,3], True) == []
-print(overlap([0,3,0,3,0,3], [1,10,0,4,0,3], True))
 assert overlap([0,3,0,3,0,3], [1,10,0,4,0,3], True) == [[0,0,0,3,0,3]]
 
 newtestres = overlap([-20, 26, -36, 17, -47, -27],[-22, 28, -29, 23, -38, 16], True)
-print(newtestres)
 assert newtestres == [[-20, 26, -36, 17, -47, -39], [-20, 26, -36, -30, -38, -27]]
 
-overlap([-20, 26, -36, 17, -47, -27],[-20, 26, -36, 17, -47, -39], True) == [[-20, 26, -36, 17, -47, -27]]
-overlap([-20, 26, -36, 17, -47, -27],[-20, 26, -36, -30, -38, -27], True) == [[-20, 26, -36, 17, -47, -27]]
+assert overlap([-20, 26, -36, 17, -47, -27],[-20, 26, -36, 17, -47, -39], True) == [[-20, 26, -36, 17, -38, -27]]
+assert overlap([-20, 26, -36, 17, -47, -27],[-20, 26, -36, -30, -38, -27], True) == [[-20, 26, -36, 17, -47, -39], [-20, 26, -29, 17, -38, -27]]
 
 nres = overlap([0,6,0,6,0,6], [6,9,2,4,2,4], True)
 assert compt(nres) == 334
-print(nres)
-#assert nres == [[0,5,0,6,0,6],[]]
 
 def procedure2(data):
     zones = []
@@ -340,7 +294,10 @@ data_test = ["on x=-20..26,y=-36..17,z=-47..7",
 "on x=-41..9,y=-7..43,z=-33..15",
 "on x=-54112..-39298,y=-85059..-49293,z=-27449..7877",
 "on x=967..23432,y=45373..81175,z=27513..53682"]
-    
+   
+data_exo = readfile("data/d22.txt")
+data_exemple = readfile("data/d22_exemple.txt")
+
 print("Node OK")
 #zones = procedure2(data_test)
 #zones = procedure2(data_test0)
