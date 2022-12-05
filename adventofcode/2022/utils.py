ae14fb3e66ab6c909a5241f444aeaa0dc8949a8b
Pierre DE VETTOR <pierre.de-vettor@bedrockstreaming.com>
2022-12-05 11:52:33 +0100

    Advent 2022 - Day 05

--- adventofcode/2022/utils.py
+++ adventofcode/2022/utils.py
@@ -10,3 +10,9 @@ def dict_plusplus(data, i, val):
         data[i] = 0
     data[i]+= val
     return data
+
+def dict_append(data, i, val):
+    if not i in data.keys():
+        data[i] = []
+    data[i].append(val)
+    return data
